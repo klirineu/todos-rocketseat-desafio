b870fb4fa75e0691582d38bf302896d201946e07
klirineu <klirineu@MacBook-Pro-de-klirineu.local>
2021-10-24 10:30:42 -0300

    concluindo segunda etapa do desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,41 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+export type EditTaskArgs = {
+  taskId: number;
+  taskNewTitle: string;
+}
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    const data = {
-      id: new Date().getTime(),
-      title: newTaskTitle,
-      done: false
+    const taskAlreadyExist = tasks.find(task => task.title === newTaskTitle)
+  
+    if(taskAlreadyExist) {
+      Alert.alert(
+        "Task já cadastrada",
+        "Você não pode cadastrar uma task com o mesmo nome",
+        [
+          {
+            text: "OK", onPress: () => console.log("OK Pressed")
+          }
+        ]
+      )
+    } else {
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+  
+      setTasks(oldState => [...oldState, data])
     }
 
-    setTasks(oldState => [...oldState, data])
   }
 
   function handleToggleTaskDone(id: number) {
@@ -32,9 +52,35 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks(oldState => oldState.filter(
-      task => task.id !== id
-    ))
+    Alert.alert(
+      "Remover Item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não", onPress: () => console.log("Não Pressed")
+        },
+        {
+          text: "Sim", onPress: () => setTasks(oldState => oldState.filter(
+            task => task.id !== id
+          ))
+        }
+      ]
+    )
+    
+  }
+
+  function handleEditTask({ taskId, taskNewTitle }: EditTaskArgs) {
+    const updateTasks = tasks.map(task => ({ ...task }))
+
+    const taskToBeUpdated = updateTasks.find(item => item.id === taskId)
+
+    if(!taskToBeUpdated) {
+      return
+    }
+
+    taskToBeUpdated. title = taskNewTitle
+    setTasks(updateTasks)
+
   }
 
   return (
@@ -47,6 +93,7 @@ export function Home() {
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
+        editTask={handleEditTask}
       />
     </View>
   )
